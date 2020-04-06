34d1027652f8a12a73ae663f434444f6c382b455
Martin Halseide <martahal@stud.ntnu.no>
2020-04-06 16:11:53 +0100

    First commit

---
+++ app/src/main/java/com/example/ambientprojecttake2/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.ambientprojecttake2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button captureImageBtn, detectTextBtn;
+    private ImageView imageView;
+    private TextView textView;
+    private Bitmap imageBitmap;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        captureImageBtn=findViewById(R.id.capture_image_btn);
+        detectTextBtn=findViewById(R.id.detect_text_btn);
+        imageView=findViewById(R.id.image_view);
+        textView=findViewById(R.id.text_display);
+
+        captureImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+                textView.setText("");
+            }
+        });
+        detectTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                detectTextFromImage();
+            }
+        });
+
+    }
+
+
+    /*
+    Documentation: https://developer.android.com/training/camera/photobasics#java
+    Methods for taking photos with external camera app.
+    Maybe modify to use CameraX and integrate camera to app later
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            this.setBitmap(imageBitmap); // Call help method to set bitmap so that it can be retrieved
+        }
+    }
+
+    private void detectTextFromImage() {
+        FirebaseVisionImage firebaseVisionImage =
+                FirebaseVisionImage.fromBitmap(getImageBitmap()); //retrieving bitmap from help function
+        FirebaseVisionTextRecognizer firebaseVisionTextRecognizer =
+                FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        firebaseVisionTextRecognizer.processImage(firebaseVisionImage).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                displayTextFromImage(firebaseVisionText);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                Log.d("Error: ", e.getMessage());
+            }
+        });
+    }
+
+    /*
+    This method probably need modification for the text to be shown in context with the usage of the app.
+     */
+    private void displayTextFromImage(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.TextBlock> blockList = firebaseVisionText.getTextBlocks();
+        if (blockList.size() == 0){
+            Toast.makeText(this, "No text found in image", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            for (FirebaseVisionText.TextBlock block : blockList){
+                String text = block.getText();
+                textView.setText(text);
+            }
+        }
+    }
+
+
+    /*
+    Help method to set and get value of global variable imageBitmap so that can be retrieved
+    */
+    private void setBitmap(Bitmap bitmap){
+        this.imageBitmap = bitmap;
+    }
+    private Bitmap getImageBitmap(){
+        return this.imageBitmap;
+    }
+}
