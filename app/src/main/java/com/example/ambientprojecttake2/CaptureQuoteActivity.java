b6a507262fec6f2962b76e67da4f906530568cf6
Martin Halseide <martahal@stud.ntnu.no>
2020-04-09 11:08:05 +0100

    added new activity CaptureQuoteActivity

---
+++ app/src/main/java/com/example/ambientprojecttake2/CaptureQuoteActivity.java
@@ -0,0 +1,13 @@
+package com.example.ambientprojecttake2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaptureQuoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_quote);
+    }
+}
